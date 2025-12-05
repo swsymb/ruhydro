7a04ee0e0de2ce338cf21cc9b69534a95dfa3c26
Alina <agera404@gmail.com>
2025-12-05 16:57:49 +0500

    Quartz sync: Dec 5, 2025, 4:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Гидропонника простыми словами",
+    pageTitle: "Гидропонника",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
