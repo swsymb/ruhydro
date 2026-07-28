ef1f522dd73e6ea0dd8eee66cf86216a57e754f0
Alina <agera404@gmail.com>
2026-07-28 14:12:31 +0500

    Quartz sync: Jul 28, 2026, 2:12 PM

---
+++ quartz.config.ts
@@ -0,0 +1,80 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "🪴 ruhydro",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "ru-RU",
+    baseUrl: "www.ruhydro.ru",  // ВАШ ДОМЕН
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          primary: "#1e293b",
+          secondary: "#334155",
+          accent: "#0ea5e9",
+          gray: "#94a3b8",
+          light: "#f8fafc",
+          lightgray: "#e2e8f0",
+          dark: "#0f172a",
+          darkgray: "#64748b",
+        },
+        darkMode: {
+          primary: "#e2e8f0",
+          secondary: "#cbd5e1",
+          accent: "#38bdf8",
+          gray: "#475569",
+          light: "#0f172a",
+          lightgray: "#1e293b",
+          dark: "#f1f5f9",
+          darkgray: "#94a3b8",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting(),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
